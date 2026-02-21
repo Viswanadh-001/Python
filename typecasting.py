95c84cb185b1e4310e0eaadf86c3fa5f2f58dd0a
Sai Viswanadh Kuppachi <kviswanadhacharan@gmail.com>
2026-02-21 16:07:05 +0530

    tc

---
+++ typecasting.py
@@ -0,0 +1,26 @@
+# s=26
+# x=int(s)
+
+# cnt=5
+# y=float(cnt)
+
+# age=25
+# z=str(age)
+
+# print(x,y,z)
+
+# Types of varaiables
+
+n=2
+f=3.4
+s="Hello Dude"
+li = [1,2,3]
+d = {'Key':'value'}
+bool = True
+
+print(type(n))
+print(type(f))
+print(type(s))
+print(type(li))
+print(type(d))
+print(type(bool))
