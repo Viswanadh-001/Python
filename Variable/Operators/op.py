8a72afc0607b95a4b00aac3ff92f6af11db14292
Sai Viswanadh Kuppachi <kviswanadhacharan@gmail.com>
2026-02-21 21:35:54 +0530

    ope

---
+++ Variable/Operators/op.py
@@ -0,0 +1,45 @@
+#Arithmetic Operator
+
+# a,b=5,2
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)#reminder
+# print(a**b)#a^b(a Power b)
+
+#Relational/comparative operator
+
+# a,b=50,20
+
+# print(a==b)
+# print(a!=b)
+# print(a>=b)
+# print(a>b)
+# print(a<=b)
+# print(a<b)
+
+#Assignment Operators
+
+num=10
+# num = num + 10
+#num += 10
+#num -= 10
+#num *= 10
+#num /= 10
+#num %= 10
+# num **= 10
+# print("num is : ",num)
+
+#Logical Operators (NOT AND OR)
+
+# a,b=50,20
+
+# print(not False)
+# print(not(a>b))
+
+val1,val2 = True, False
+print("AND Operator: ",val1 and val2 )
+
+print("OR Operator: ",val1 and val2 )
